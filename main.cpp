cbdbc670db225384a08ac98851f1cbe0dc061a55
David Bullock <davidwbullock@gmail.com>
2021-04-24 20:39:38 -0700

    Added player death.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 /*******************************************************************************************
-*
-*  Deeper and deeper:
-*     Deep thoughts (jack handy, the game).
-*     Galaxy -> Solar System -> Earth -> Human -> Cell -> Atom -> Craziness
-*     Deep into the mind
-*     Deep into the ocean.
-*     Deep in a dungeon.
-*     Deep conspiracy.
-*     Deep learning.
-*     Deep crust pizza.
-*     Deep neural nets - you pick the weights.
+* (c) 2021 DancesWE
+* 
+* Die when red hits you.
+* Sparks on player
+* Phase in/out on energy zone
+* Procedural maps
+* Powerup for depth of view.
+* Powerup for speed.
+* 
+* Music
+* Intro screen
+* Momentum for play
 *
 ********************************************************************************************/
 
 #include <chrono>
 #include <thread>
+#include <functional>
+#include <iostream>
 #include <sstream>
 #include <vector>
 #include <memory>
@@ -186,6 +189,85 @@ void playerCornersInSimSpace(
     p3 = { playerSlice - playerHeightSliceDirDiv2, playerPosition - playerWidthInSliceDiv2 };
 }
 
+class SafeImage
+{
+    public:
+        SafeImage(const SafeImage&) = delete;
+        SafeImage& operator=(SafeImage const&) = delete;
+
+        SafeImage(const char fname[]) {
+            image = LoadImage(fname);
+            colorArr = LoadImageColors(image);
+        }
+        ~SafeImage() {
+            UnloadImageColors(colorArr);
+            UnloadImage(image);
+        }
+        Color& color(int x, int y) const { return colorArr[image.width * y + x]; }
+        Color* colorArr;
+        Image image;
+};
+
+class LevelTransformer
+{
+    LevelTransformer(int sliceSize, int sliceWidth, int sliceHeight, float worldWidth, float worldHeight)
+        : sliceSize(sliceSize)
+        , sliceWidth(sliceWidth)
+        , sliceHeight(sliceHeight)
+        , worldWidth(worldWidth)
+        , worldHeight(worldHeight) {
+    }
+
+    // convert image slice/index into slice to a point; gives left-top most world point of element
+    Vector3 simToWorld(float slice, int index) const {
+        // ensure index in range
+        int modulo = sliceSize;
+        while (index < 0) {
+            index += modulo; // yikes
+        }
+        index = index % modulo;
+
+        const float fSliceWidth = static_cast<float>(sliceWidth);
+        const float fSliceHeight = static_cast<float>(sliceHeight);
+        if (index < sliceWidth) {
+            float t1 = float(index) / fSliceWidth;
+            return(Vector3{ worldWidth * t1, 0.0f, slice });
+        }
+
+        index -= sliceWidth;
+        if (index < sliceHeight) {
+            float t1 = float(index) / fSliceHeight;
+            return(Vector3{ worldWidth, worldHeight * t1, slice });
+        }
+
+        index -= sliceHeight;
+        if (index < sliceWidth) {
+            float t1 = 1.0f - float(index) / fSliceWidth;
+            return(Vector3{ worldWidth * t1, worldHeight, slice });
+        }
+
+        index -= sliceWidth;
+        float t1 = 1.0f - float(index) / fSliceHeight;
+        return(Vector3{ 0, worldHeight * t1, slice });
+    }
+
+    Vector3 simToWorldFloat(float slice, float index) const {
+        float index1 = floorf(index);
+        float t = index - index1;
+        int index1Int = static_cast<int>(index1);
+
+        Vector3 p0 = simToWorld(slice, index1Int);
+        Vector3 p1 = simToWorld(slice, index1Int + 1);
+        return Vector3Lerp(p0, p1, t);
+    }
+
+    const int sliceSize;
+    const int sliceWidth;
+    const int sliceHeight;
+    const float worldWidth;
+    const float worldHeight;
+};
+
 class LevelGeometry : public Thing
 {
 public:
@@ -195,6 +277,7 @@ public:
         , playerColor({ 45,227,191,255 })
         , playerSlice(10.0f)
         , numSlices(3000)
+        , dangerZone(startingDangerZone)
     {
         geom.resize(numSlices);
         for (int ii = 0; ii < numSlices; ++ii) {
@@ -288,27 +371,54 @@ public:
         return false;
     }
 
-    int numSlices = 3000;
-    const int sliceWidth = 80;
-    const int sliceHeight = 60;
+    float dangerZoneT() {
+        float t = 1.0f - (playerSlice - float(dangerZone)) / float(numWarningZones);
+        return std::min(1.0f, std::max(0.0f, t));
+    }
+    void loadBackgroundImage(const char fname[]) {
+        backgroundImage.reset(new SafeImage(fname));
+    }
+
+    // sim space constants
+    int numSlices = 3000; // number of individual slices down the 'z' axis
+
+    //          sliceWidth
+    //    a------------------+
+    //    b                  |
+    //    |                  | sliceHeight
+    //    +------------------+
+    //
+    //    a - index 0
+    //    b - index sliceSize-1
+    const int sliceWidth = 80; // number of pixels in x axis
+    const int sliceHeight = 60; // number of pixels in y axis
+    const int sliceSize = sliceWidth * 2 + sliceHeight * 2;
+
     const float playerHeightSliceDirDiv2 = 0.5f;
     const float playerWidthInSliceDiv2 = 0.5f;
-    //const int sliceWidth = 80;
-    //const int sliceHeight = 60;
-    const int sliceSize = sliceWidth * 2 + sliceHeight * 2;
-    Color playerColor;
 
     // grid space
     std::vector< std::vector<unsigned char> > geom;
     float playerSlice; // which slice the player is on
     float playerPosition; // position on slice
 
-    // items in world space
+    // world space
     const float                         worldWidth = static_cast<float>(400);
     const float                         worldHeight = static_cast<float>(300);
     std::vector< std::vector<Vector3> > worldGeom; // position of top-left point of each geom pixel; includes extra row/element to bake end 
 
+    // Display Constants
+    Color playerColor;
+    const float slicesBeforePlayer = 20.0f;
+    const float slicesPerScreen = 30.0f;
 
+    // Danger Zone
+    int dangerZone; // zone in which player is killed
+    const int startingDangerZone = -40;
+    int numWarningZones = 50;
+
+    // Visuals
+    std::unique_ptr<SafeImage> backgroundImage;
 
   private:
 
@@ -368,8 +478,94 @@ public:
         pos = Vector2Multiply(pos, screenCenter);
         pos = Vector2Add(pos, screenCenter);
         return pos;
-    }
+      }
 
+      void DrawPlayer(const Vector2& screenCenter, float sliceAtCenter)
+      {
+          SimSpacePosition sp0, sp1, sp2, sp3;
+          playerCornersInSimSpace(playerSlice, playerPosition, playerWidthInSliceDiv2, playerHeightSliceDirDiv2, sp0, sp1, sp2, sp3);
+          const Vector3 player0World = toWorldFloat(sp0.slice, sp0.positionInSlice);
+          const Vector3 player1World = toWorldFloat(sp1.slice, sp1.positionInSlice);
+          const Vector3 player2World = toWorldFloat(sp2.slice, sp2.positionInSlice);
+          const Vector3 player3World = toWorldFloat(sp3.slice, sp3.positionInSlice);
+
+          const Vector2 p0 = toScreen(player0World, sliceAtCenter, slicesPerScreen, screenCenter);
+          const Vector2 p1 = toScreen(player1World, sliceAtCenter, slicesPerScreen, screenCenter);
+          const Vector2 p2 = toScreen(player2World, sliceAtCenter, slicesPerScreen, screenCenter);
+          const Vector2 p3 = toScreen(player3World, sliceAtCenter, slicesPerScreen, screenCenter);
+          DrawTriangle(p0, p1, p2, playerColor);
+          DrawTriangle(p0, p2, p3, playerColor);
+      }
+
+      void DrawAllGrid(const Vector2& screenCenter, float sliceAtCenter, std::function<void(int, int, Color&, bool&)> ColorCallback)
+      {
+          const int numSlicesToIterate = static_cast<int>(slicesPerScreen) + 2;
+          const int sliceAtCenterInt = static_cast<int>(sliceAtCenter);
+
+          for (int ii = 0; ii < numSlicesToIterate; ++ii) {
+              int currSliceIndex = sliceAtCenterInt - ii;
+              if (currSliceIndex >= 0 && currSliceIndex < (static_cast<int>(worldGeom.size()) - 1)) {
+                  assert(size_t(currSliceIndex) < worldGeom.size());
+                  const std::vector<Vector3>& sliceWorld = worldGeom[currSliceIndex];
+                  const std::vector<Vector3>& nextSliceWorld = worldGeom[currSliceIndex + 1];
+                  assert(sliceWorld.size() <= size_t(sliceSize + 1));
+                  //       b11-p3--------p2-b12
+                  //        |\   \xxxxxx/   /|
+                  //        | a11-p0--p1-a12 |
+                  //        |  |          |  |
+                  //        | a21--------a22 |
+                  //        |/              \|
+                  //       b21--------------b22
+                  for (int jj = 0; jj < sliceSize; jj++) {
+                      const Vector2 p0 = toScreen(sliceWorld[jj], sliceAtCenter, slicesPerScreen, screenCenter);
+                      const Vector2 p1 = toScreen(sliceWorld[jj + 1], sliceAtCenter, slicesPerScreen, screenCenter);
+                      const Vector2 p2 = toScreen(nextSliceWorld[jj + 1], sliceAtCenter, slicesPerScreen, screenCenter);
+                      const Vector2 p3 = toScreen(nextSliceWorld[jj], sliceAtCenter, slicesPerScreen, screenCenter);
+                      Color col;
+                      bool render;
+                      ColorCallback(currSliceIndex, jj, col, render);
+                      if (render) {
+                          DrawTriangle(p2, p1, p0, col);
+                          DrawTriangle(p3, p2, p0, col);
+                      }
+                  }
+              }
+          }
+      }
+
+      void DrawGridSolid(const Vector2& screenCenter, float sliceAtCenter, const Color& col)
+      {
+          const int numSlicesToIterate = static_cast<int>(slicesPerScreen) + 2;
+          const int sliceAtCenterInt = static_cast<int>(sliceAtCenter);
+
+          for (int ii = 0; ii < numSlicesToIterate; ++ii) {
+              int currSliceIndex = sliceAtCenterInt - ii;
+              if (currSliceIndex >= 0 && currSliceIndex < (static_cast<int>(worldGeom.size()) - 1)) {
+                  assert(size_t(currSliceIndex) < worldGeom.size());
+                  const std::vector<Vector3>& sliceWorld = worldGeom[currSliceIndex];
+                  const std::vector<Vector3>& nextSliceWorld = worldGeom[currSliceIndex + 1];
+                  assert(sliceWorld.size() <= size_t(sliceSize + 1));
+                  const std::vector<unsigned char>& slice = geom[currSliceIndex];
+                  //       b11-p3--------p2-b12
+                  //        |\   \xxxxxx/   /|
+                  //        | a11-p0--p1-a12 |
+                  //        |  |          |  |
+                  //        | a21--------a22 |
+                  //        |/              \|
+                  //       b21--------------b22
+                  for (int jj = 0; jj < sliceSize; jj++) {
+                      if (!slice[jj]) continue;
+
+                      const Vector2 p0 = toScreen(sliceWorld[jj], sliceAtCenter, slicesPerScreen, screenCenter);
+                      const Vector2 p1 = toScreen(sliceWorld[jj + 1], sliceAtCenter, slicesPerScreen, screenCenter);
+                      const Vector2 p2 = toScreen(nextSliceWorld[jj + 1], sliceAtCenter, slicesPerScreen, screenCenter);
+                      const Vector2 p3 = toScreen(nextSliceWorld[jj], sliceAtCenter, slicesPerScreen, screenCenter);
+                      DrawTriangle(p2, p1, p0, col);
+                      DrawTriangle(p3, p2, p0, col);
+                  }
+              }
+          }
+      }
 };
 
 void LevelGeometry::doRender()
@@ -377,99 +573,135 @@ void LevelGeometry::doRender()
     const float centerX = static_cast<float>(GetScreenWidth() / 2);
     const float centerY = static_cast<float>(GetScreenHeight() / 2);
     const Vector2 screenCenter = { centerX, centerY };
-
-    const float slicesBeforePlayer = 15.0f;
     const float sliceAtCenter = playerSlice + slicesBeforePlayer;
-    const float slicesPerScreen = 20.0f;
-    const int numSlicesToIterate = static_cast<int>(slicesPerScreen) + 2;
-    const int sliceAtCenterInt = static_cast<int>(sliceAtCenter);
 
-    // draw grid
-    for (int ii = 0; ii < numSlicesToIterate; ++ii) {
-        int currSliceIndex = sliceAtCenterInt - ii;
-        if (currSliceIndex >= 0 && currSliceIndex < (static_cast<int>(worldGeom.size()) - 1)) {
-            assert(size_t(currSliceIndex) < worldGeom.size());
-            const std::vector<Vector3>& sliceWorld = worldGeom[currSliceIndex];
-            const std::vector<Vector3>& nextSliceWorld = worldGeom[currSliceIndex + 1];
-            assert(sliceWorld.size() <= size_t(sliceSize + 1));
-            const std::vector<unsigned char>& slice = geom[currSliceIndex];
-            //       b11-p3--------p2-b12
-            //        |\   \xxxxxx/   /|
-            //        | a11-p0--p1-a12 |
-            //        |  |          |  |
-            //        | a21--------a22 |
-            //        |/              \|
-            //       b21--------------b22
-            for (int jj = 0; jj < sliceSize; jj++) {
-                if (!slice[jj]) continue;
 
-                const Vector2 p0 = toScreen(sliceWorld[jj], sliceAtCenter, slicesPerScreen, screenCenter);
-                const Vector2 p1 = toScreen(sliceWorld[jj + 1], sliceAtCenter, slicesPerScreen, screenCenter);
-                const Vector2 p2 = toScreen(nextSliceWorld[jj + 1], sliceAtCenter, slicesPerScreen, screenCenter);
-                const Vector2 p3 = toScreen(nextSliceWorld[jj], sliceAtCenter, slicesPerScreen, screenCenter);
-                DrawTriangle(p2, p1, p0, RED);
-                DrawTriangle(p3, p2, p0, RED);
+    //DrawGrid(screenCenter, sliceAtCenter, [](int, int, Color& col, bool& render) {col = RED; render = true; });
+    if (backgroundImage.get()) {
+        DrawAllGrid(screenCenter, sliceAtCenter, [=](int slice, int sliceIndex, Color& col, bool& render) {
+                render = false;
+                if (slice >= 0 && slice < backgroundImage->image.width && sliceIndex >= 0 && sliceIndex < backgroundImage->image.height) {
+                    col = backgroundImage->color(slice, sliceIndex);
+                    render = true;
+                }
+            });
+    }
+    DrawGridSolid(screenCenter, sliceAtCenter, RED);
+    DrawPlayer(screenCenter, sliceAtCenter);
+    DrawAllGrid(screenCenter, sliceAtCenter, [=](int slice, int sliceIndex, Color& col, bool& render) {
+            render = false;
+            if (slice <= dangerZone) {
+                col = RED;
+                render = true;
+                col.a = static_cast<unsigned char>(255.0f * 0.4f);
             }
-        }
-    }
-
-    // draw player
-    {
-        SimSpacePosition sp0, sp1, sp2, sp3;
-        playerCornersInSimSpace(playerSlice, playerPosition, playerWidthInSliceDiv2, playerHeightSliceDirDiv2, sp0, sp1, sp2, sp3);
-        const Vector3 player0World = toWorldFloat(sp0.slice, sp0.positionInSlice);
-        const Vector3 player1World = toWorldFloat(sp1.slice, sp1.positionInSlice);
-        const Vector3 player2World = toWorldFloat(sp2.slice, sp2.positionInSlice);
-        const Vector3 player3World = toWorldFloat(sp3.slice, sp3.positionInSlice);
-
-        const Vector2 p0 = toScreen(player0World, sliceAtCenter, slicesPerScreen, screenCenter);
-        const Vector2 p1 = toScreen(player1World, sliceAtCenter, slicesPerScreen, screenCenter);
-        const Vector2 p2 = toScreen(player2World, sliceAtCenter, slicesPerScreen, screenCenter);
-        const Vector2 p3 = toScreen(player3World, sliceAtCenter, slicesPerScreen, screenCenter);
-        DrawTriangle(p0, p1, p2, playerColor);
-        DrawTriangle(p0, p2, p3, playerColor);
-    }
+            else if (slice - dangerZone <= numWarningZones) {
+                float t = 1 - float(slice - dangerZone) / float(numWarningZones);
+                col = YELLOW;
+                col.a = static_cast<unsigned char>(255.0f * 0.4f * t);
+                render = true;
+            }
+        });
 }
 
 class GameState
 {
 public:
-    enum class PlayerDirection { CCW, CW, IN, OUT };
+    enum class PlayerDirection { CCW, CW, IN, OUT, NONE };
 
     GameState()
-      : playerDirection(PlayerDirection::IN)
-      , simTick(0)
+        : playerDirection(PlayerDirection::IN)
+        , simTick(0)
+        , debugText(WHITE, {30, 30})
+        , pausedText(WHITE, { 370, 300 }, "PAUSED")
+        , paused(false)
+        , playerDead(false)
     {
         lg.loadLevelFromImage("Content/test_level.png");
+        lg.loadBackgroundImage("Content/test_level_bg.png");
+        bump = LoadSound("Content/hitwall.wav");
+        dangerSound = LoadSound("Content/bg_buzz.wav");
+        deathSound = LoadSound("Content/explosion.wav");
+
+        debugText.display = false;
+        pausedText.display = false;
+    }
+
+    ~GameState()
+    {
+        UnloadSound(bump);
     }
 
     void Sim(float simTimeSeconds) {
         const float playerHorizontalSpeed = 0.75;
         const float playerVerticalSpeed = 0.5;
 
+        if (IsKeyPressed(KEY_D)) debugText.display = !debugText.display;
+        if (IsKeyPressed(KEY_P)) {
+            paused = !paused;
+            pausedText.display = paused;
+        }
+        if (paused || playerDead)
+            return;
+
         if (IsKeyDown(KEY_UP)) playerDirection = PlayerDirection::IN;
         if (IsKeyDown(KEY_DOWN)) playerDirection = PlayerDirection::OUT;
         if (IsKeyDown(KEY_LEFT)) playerDirection = PlayerDirection::CCW;
         if (IsKeyDown(KEY_RIGHT)) playerDirection = PlayerDirection::CW;
+        bool wasCollision = false;
         if (playerDirection == PlayerDirection::IN) {
-            lg.incrPlayerSlice(playerVerticalSpeed);
+            wasCollision = lg.incrPlayerSlice(playerVerticalSpeed);
         }
         else if (playerDirection == PlayerDirection::OUT) {
-            lg.incrPlayerSlice(-playerVerticalSpeed);
+            wasCollision = lg.incrPlayerSlice(-playerVerticalSpeed);
         }
         else if (playerDirection == PlayerDirection::CCW) {
-            lg.incrPlayerPosition(-playerHorizontalSpeed);
+            wasCollision = lg.incrPlayerPosition(-playerHorizontalSpeed);
         }
         else if (playerDirection == PlayerDirection::CW) {
-            lg.incrPlayerPosition(playerHorizontalSpeed);
+            wasCollision = lg.incrPlayerPosition(playerHorizontalSpeed);
         }
+        if (wasCollision) {
+            PlaySound(bump);
+            playerDirection = PlayerDirection::NONE;
+        }
+
+        if (simTick % 12 == 0) {
+            lg.dangerZone++;
+        }
+
+        float dangerValue = lg.dangerZoneT();
+        //std::cout << GetMusicTimePlayed(dangerSound) << std::endl;
+        if (dangerValue < 0.01f) {
+            if (IsSoundPlaying(dangerSound))
+                StopSound(dangerSound);
+            SetSoundVolume(dangerSound, 0.0f);
+        }
+        else {
+            if (! IsSoundPlaying(dangerSound))
+                PlaySound(dangerSound);
+            SetSoundVolume(dangerSound, dangerValue * 0.4);
+        }
+
+        if (static_cast<int>(floorf(lg.playerSlice)) <= lg.dangerZone) {
+            playerDead = true;
+            PlaySound(deathSound);
+        }
+
+
+        std::stringstream ss;
+        ss << "Sim tick " << simTick << ", Player: (" << lg.playerSlice << ", " << lg.playerPosition << ")";
+        debugText.text = ss.str();
         simTick++;
     }
+
     void Render() {
         BeginDrawing();
         ClearBackground(BLACK);
 
         lg.render();
+        debugText.render();
+        pausedText.render();
 
         EndDrawing();
     }
@@ -477,6 +709,14 @@ public:
     int simTick;
     LevelGeometry lg;
     PlayerDirection playerDirection;
+    Text debugText;
+    Text pausedText;
+    bool paused;
+    bool playerDead;
+
+    Sound bump;
+    Sound dangerSound;
+    Sound deathSound;
 };
 
 
